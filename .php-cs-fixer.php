4202b8fe897fef59e2269ede07f0066f87493824
Vasyl Sokolyk <vsokolyk@gmail.com>
2022-03-08 19:48:03 +0200

    used one rules-config.php file for export all configs

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,12 +1,3 @@
 <?php
 
-require_once __DIR__ . '/src/loader.php';
-
-use JazzMan\PhpCsFixerRules\Config;
-use JazzMan\PhpCsFixerRules\Finder;
-
-$finder = Finder::getFinder(__DIR__);
-
-$config = (new Config());
-
-return $config->setFinder($finder);
+return require_once __DIR__ . '/src/rules-config.php';
