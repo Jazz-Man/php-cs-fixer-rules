63af870f7f1794615c59196b534b8868e55686d8
Vasyl Sokolyk <vsokolyk@gmail.com>
2022-03-12 23:18:19 +0200

    fix: disable FixerName.php file

--- src/rules-config.php
+++ src/rules-config.php
@@ -4,8 +4,6 @@ namespace JazzMan\PhpCsFixerRules;
 
 use PhpCsFixer\ConfigInterface;
 
-require_once __DIR__.'/FixerName.php';
-
 require_once __DIR__.'/Finder.php';
 
 require_once __DIR__.'/Config.php';
