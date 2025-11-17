a5aefb91cf37fef51ffdc9d9354c75de2df138a6
Vasyl Sokolyk <vsokolyk@gmail.com>
2025-11-17 22:58:30 +0200

    Replace PHP version rules with autoPHPMigration rules

--- src/Config.php
+++ src/Config.php
@@ -25,8 +25,8 @@ final class Config extends \PhpCsFixer\Config {
             '@PSR12' => true,
             '@PhpCsFixer' => true,
             '@PhpCsFixer:risky' => true,
-            '@PHP8x2Migration' => true,
-            '@PHP8x0Migration:risky' => true,
+            '@autoPHPMigration' => true,
+            '@autoPHPMigration:risky' => true,
             'WeDevs/space_inside_parenthesis' => true,
             'WeDevs/blank_line_after_class_opening' => true,
             'blank_line_before_statement' => [
